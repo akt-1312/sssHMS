9c8d0eae49efa449a5fe4a72e18fbf7f8b4d2c88
akt-1312 <55472602+akt-1312@users.noreply.github.com>
2020-01-08 09:54:59 +0630

    Fix Error

--- sssHMS/sssHMS/Controllers/AmbulancesController.cs
+++ sssHMS/sssHMS/Controllers/AmbulancesController.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Hospital.Data;
 using Hospital.Models;
+using sssHMS.Data;
 
 namespace Hospital.Controllers
 {
